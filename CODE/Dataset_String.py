45d0be163ec36d5f385e49d43be3985109e16797
DEEP DEY <63514087+Deep-Dey@users.noreply.github.com>
2020-07-14 17:23:39 +0530

    Removed web-scrapping due to website issue

--- CODE/Dataset_String.py
+++ CODE/Dataset_String.py
@@ -7,17 +7,14 @@ def main(n):
     import requests
     import csv
 
-    source = requests.get('https://www.ef.com/wwen/english-resources/english-vocabulary/top-3000-words').text
-
-    soup = BeautifulSoup(source, 'lxml')
     dataset=list()
-    titles=soup.find('div', class_='field-item even')
-    for data in titles.find_all('p'):
-        dataset.append(data.text)
+    with open('String_Dataset.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            dataset.append(row[0])
 
-    data=dataset[1].split('\n\t')
-    data.reverse()
-    itemset=data[0:n]
+    dataset.reverse()
+    itemset=dataset[0:n]
     for i in range(len(itemset)):
         itemset[i]=itemset[i].lower()
     
